be1cf999501649fb995514db0899811cc95ce83e
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2022-01-20 23:49:50 -0400

    Almost finished
    We added a lot of things here

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float reloadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "MainSurface")
+        {
+            Debug.Log("ouch!");
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", reloadDelay);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
